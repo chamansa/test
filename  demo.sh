9402d184b36510b3dabf13e34e521b96f7697ade
chamansa <39539280+chamansa@users.noreply.github.com>
2025-04-16 11:56:26 -0500

    Add files via upload

---
+++  demo.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+echo "Jupytor shell "
+Compressed shell Echo Guide to Running Python Scripts/Notebooks.
+echo Step 1 – open terminal, Open the Demo folder directory:
+cd /Users/chaman/activeFolder/\ \ \ \ ClassesSP2025/\ \ \ \ CS5000Shoaib/Demo
+echo Step 2 - Activate the environment:
+python3 -m venv venv
+source venv/bin/activate
+echo Step 3 - Launch Jupyter Notebook:
+pip3 install notebook
+jupyter notebook
